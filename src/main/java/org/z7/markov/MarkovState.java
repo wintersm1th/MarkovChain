c2d804bfcb5027adebe13093894113bec63f2f86
Sergey Konoplich <s.konopl@yandex.ru>
2023-02-01 17:21:46 +0300

    feat: add type restriction for MerkovState

--- src/main/java/org/z7/markov/MarkovState.java
+++ src/main/java/org/z7/markov/MarkovState.java
@@ -1,5 +1,5 @@
 package org.z7.markov;
 
-public interface MarkovState<C, CONCRETE> {
+public interface MarkovState<C, CONCRETE extends MarkovState<C, CONCRETE>> {
     void addTransition(CONCRETE state, float probability);
 }
