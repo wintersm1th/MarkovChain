808d499e85d0d2ee0d5ba431f19e5926bf8b6f44
Sergey Konoplich <s.konopl@yandex.ru>
2023-02-07 16:09:47 +0300

    feat: add undirected graph implementation

--- modules/graphs_simplified/src/main/java/org/z7/graphs_simplified/UndirectedGraph.java
+++ modules/graphs_simplified/src/main/java/org/z7/graphs_simplified/UndirectedGraph.java
@@ -1,4 +1,33 @@
 package org.z7.graphs_simplified;
 
+import org.z7.util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class UndirectedGraph<V extends Vertex, E extends UndirectedTransition<V>> extends AbstractGraph<V, E> {
+    private final HashMap<V, ArrayList<V>> transitions = new HashMap<>();
+
+    @Override
+    public void addVertex(V vertex) {
+        transitions.put(vertex, new ArrayList<>());
+    }
+
+    @Override
+    public void addTransition(E transition) {
+        Pair<V, V> ends = transition.getVertices();
+
+        V firstEnd = ends.getFirst();
+        V secondEnd = ends.getSecond();
+
+        transitions.get(firstEnd).add(secondEnd);
+        transitions.get(secondEnd).add(firstEnd);
+    }
+
+    @Override
+    public List<V> getAdjacentFor(V target) {
+        return transitions.get(target);
+    }
+
 }
