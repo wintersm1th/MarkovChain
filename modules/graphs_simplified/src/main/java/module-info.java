fe689ce9fcd8cf0af8fad52d932c88c5f44bf154
Sergey Konoplich <s.konopl@yandex.ru>
2023-02-10 15:49:01 +0300

    fix: rename module

--- modules/graphs_simplified/src/main/java/module-info.java
+++ modules/graphs_simplified/src/main/java/module-info.java
@@ -1,3 +1,12 @@
 module org.z7.graphs_simplified {
     requires org.z7.util;
+
+    exports org.z7.graphs_simplified.edges;
+    exports org.z7.graphs_simplified.edges.intf;
+
+    exports org.z7.graphs_simplified.graphs;
+    exports org.z7.graphs_simplified.graphs.intf;
+
+    exports org.z7.graphs_simplified.vertices;
+    exports org.z7.graphs_simplified.vertices.intf;
 }
