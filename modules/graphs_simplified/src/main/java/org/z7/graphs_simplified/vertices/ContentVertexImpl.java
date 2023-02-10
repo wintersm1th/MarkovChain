cc04e18f9f14687073c7d0863845f56cb3628351
Sergey Konoplich <s.konopl@yandex.ru>
2023-02-10 15:49:48 +0300

    feat: add constructors for ContentVertexImpl

--- modules/graphs_simplified/src/main/java/org/z7/graphs_simplified/vertices/ContentVertexImpl.java
+++ modules/graphs_simplified/src/main/java/org/z7/graphs_simplified/vertices/ContentVertexImpl.java
@@ -5,6 +5,12 @@ import org.z7.graphs_simplified.vertices.intf.ContentVertex;
 public class ContentVertexImpl<C> extends AbstractVertex implements ContentVertex<C> {
     C content;
 
+    public ContentVertexImpl() { }
+
+    public ContentVertexImpl(C content) {
+        this.content = content;
+    }
+
     public C getContent() {
         return content;
     }
